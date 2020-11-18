7f960c71f19bccac91b0c4049356effe52c28b20
Elena Shevtsova <helena_shevts@mail.ru>
2020-11-18 16:09:16 +0300

    add new test, change test name in square.test file

--- src/components/stepHistory/stepHistory.test.js
+++ src/components/stepHistory/stepHistory.test.js
@@ -1,27 +1,22 @@
 import React from 'react';
 import {Provider} from "react-redux";
 import {configure, mount} from 'enzyme';
-import {Square} from "./square";
+import {StepHistory} from './index';
 import {createStore} from "redux";
 import {rootReducer, initialState} from "../../redux/reducers";
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({adapter: new Adapter()});
-const store = createStore(rootReducer, initialState);
 
-describe('Square Component', () => {
-    const squareComponent = mount(<Provider store={store}><Square/></Provider>);
+const store = createStore(rootReducer,initialState);
 
-    it('button has attribute disabled', () => {
-        expect(squareComponent.find('button').props()).toHaveProperty('disabled');
-    });
-
+describe('stepHistory Component', () => {
     it('click on the button calls the function', () => {
         const useDispatchSpy = jest.spyOn(jest.requireActual('react-redux'), 'useDispatch');
         const mockDispatchFn = jest.fn();
         useDispatchSpy.mockReturnValue(mockDispatchFn);
-        squareComponent.find('button').simulate('click');
+        const stepHistoryComponent = mount(<Provider store={store}><StepHistory/></Provider>);
+        stepHistoryComponent.find('button').simulate('click');
         expect(useDispatchSpy).toBeCalledTimes(1);
-
     });
 });
