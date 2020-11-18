a597ba372eecff0d981fa62fdd6d4be8c45c7bb3
Elena Shevtsova <helena_shevts@mail.ru>
2020-11-19 00:17:32 +0300

    add test on reducer

--- src/redux/reducers.test.js
+++ src/redux/reducers.test.js
@@ -1,4 +1,5 @@
 import {rootReducer, initialState} from "./reducers";
+//import {ACTION_CHANGE_STEP} from "./actions";
 //import * as types from './actions';
 
 describe('game reducer', () => {
@@ -6,9 +7,38 @@ describe('game reducer', () => {
     it('should return initial state', () => {
         expect(rootReducer(undefined, {})).toEqual(initialState);
     });
-    //
-    // it('should add value in history', () => {
-    //     expect(rootReducer({}, {type: types.ACTION_MAKE_A_MOVE })).toEqual({
-    //     });
-    // });
+
+    it('should handle action ACTION_CHANGE_STEP', () => {
+        expect(rootReducer({
+                history: [{
+                    squares:
+                        [null, null, null, null, null, null, null, null, null]
+                }, {
+                    squares:
+                        ['X', null, null, null, null, null, null, null, null]
+                }, {
+                    squares:
+                        ['X', 'O', null, null, null, null, null, null, null]
+                },{
+                    squares:
+                        ['X', 'O', "X", null, null, null, null, null, null]
+                }],
+                stepNumber:3,
+                xIsNext: false,
+                disabled: false,
+            }, {type: 'ACTION_CHANGE_STEP', payload:{ stepNumber: 1,
+                xIsNext: false,}}
+            )).toEqual(   {
+            history: [{
+                squares:
+                    [null, null, null, null, null, null, null, null, null]
+            }, {
+                squares:
+                    ['X', null, null, null, null, null, null, null, null]
+            }],
+            xIsNext: false,
+            stepNumber: 1,
+            disabled: false,
+        });
+    });
 });
