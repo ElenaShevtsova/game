98ca0c3f197f95598c35a73746b7a41d544a9678
Elena Shevtsova <helena_shevts@mail.ru>
2020-11-29 19:52:23 +0300

    add type in calculateWinner function

--- src/utils/calculateWinner/index.tsx
+++ src/utils/calculateWinner/index.tsx
@@ -1,4 +1,4 @@
-export function calculateWinner(squares: string[]) {
+export function calculateWinner(squares: (string | null)[]): null | undefined {
     const lines = [
         [0, 1, 2],
         [3, 4, 5],
